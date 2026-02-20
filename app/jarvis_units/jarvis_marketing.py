745b39a29cd530866a366fe287e5d63093b26b9d
1A1yush9 <97648794+1A1yush9@users.noreply.github.com>
2026-02-20 15:22:18 +0530

    "Add missing jarvis_marketing module"

---
+++ app/jarvis_units/jarvis_marketing.py
@@ -0,0 +1,9 @@
+class JarvisMarketing:
+    def __init__(self):
+        self.name = "Jarvis Marketing"
+
+    def execute(self, task=None):
+        return {
+            "status": "ok",
+            "message": "JarvisMarketing stub running"
+        }
