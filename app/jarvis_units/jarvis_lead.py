917a7ec69ca848cdde5e83bbeb3d5176581d63d9
1A1yush9 <97648794+1A1yush9@users.noreply.github.com>
2026-02-20 15:17:26 +0530

    "Add missing jarvis_units package"

---
+++ app/jarvis_units/jarvis_lead.py
@@ -0,0 +1,9 @@
+class JarvisLead:
+    def __init__(self):
+        self.name = "Jarvis Lead"
+
+    def execute(self, task=None):
+        return {
+            "status": "ok",
+            "message": "JarvisLead stub running"
+        }
